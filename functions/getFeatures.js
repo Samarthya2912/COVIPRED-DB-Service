f288626eda9139c1c451bb0ef73b916833075efd
Samarthya Singh <samarthya55@gmail.com>
2022-10-23 10:01:45 +0530

    Added user model and getFeatures function

---
+++ functions/getFeatures.js
@@ -0,0 +1,6 @@
+const getFeatures = () => {
+    return ['cough', 'fever', 'sore_throat', 'shortness_of_breath',
+    'head_ache', 'age_60_and_above', 'gender', 'test_indication'];
+}
+
+module.exports = getFeatures;
---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    aadhar: { type: String, required: true, unique: true },
+    time: { type: Date, default: Date.now },
+    symptoms: {
+        cough: { type: Boolean, default: false },
+        fever: { type: Boolean, default: false },
+        sore_throat: { type: Boolean, default: false },
+        shortness_of_breath: { type: Boolean, default: false },
+        head_ache: { type: Boolean, default: false },
+        age_60_and_above: { type: Boolean, default: false },
+        gender: { type: Boolean, default: false },
+        test_indication: { type: Boolean, default: false },
+    },
+    risk: { type: Number, default: -1 },
+    slot_assigned: { type: Boolean, default: false },
+    slot_id: { type: mongoose.Schema.Types.ObjectId, ref: 'slot', default: null }
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
